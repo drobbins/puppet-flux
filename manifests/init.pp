bc108aea1c47a5878cf8a8e9fe5b8970ef26bd06
David Robbins <robbins.david@gmail.com>
2014-06-12 07:57:17 -0500

    Initial manifest and test

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,11 @@
-# This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+# Installs Flux
+#
+# Flux makes the color of your computer's display adapt to the
+# time of day, warm at night and like sunlight during the day.
+# https://justgetflux.com/
+class flux {
+  package { 'flux':
+      source   => 'https://justgetflux.com/mac/Flux30.zip',
+      provider => 'compressed_app',
+  }
 }
